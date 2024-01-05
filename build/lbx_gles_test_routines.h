ceda0f5a9d4d89d3441721394d4aee89e0ae6e45
cymon <cymon@litbig.com>
2024-01-05 15:35:38 +0900

    누락된 파일 추가

---
+++ build/lbx_gles_test_routines.h
@@ -0,0 +1,16 @@
+#ifndef lbx_gles2_test_routinesH
+#define lbx_gles2_test_routinesH
+
+#include "lbx.h"
+
+i32_t test(void)
+{
+
+
+
+
+    return 0;
+}
+
+#endif //#ifndef lbx-gles2_test_routinesH
+
