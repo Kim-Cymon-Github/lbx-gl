d7f22a2695ac5c88e9f7f30aaa26f11ae208fe8b
cymon <cymon@litbig.com>
2024-05-02 17:09:11 +0900

    디렉토리명, 솔루션 이름 변경

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,16 @@
-@echo off
+﻿@echo off
 chcp 65001
+@echo off
+cls
+rem 현재 스크립트가 위치한 디렉토리의 경로 취득
+set "SCRIPT_PATH=%~dp0"
+echo %SCRIPT_PATH%
+rem 디렉토리 경로에서 마지막 디렉토리 이름을 추출
+for %%I in ("%SCRIPT_PATH%.") do set "MODULE_NAME=%%~nxI"
 
+set "SUB_DIR=%MODULE_NAME:*lbx-=%%"
+
+echo ### Deploying %MODULE_NAME% ... (SubDir: %SUB_DIR%)
 rem 전달된 파일이 존재하는지 확인합니다.
 set "VERSION_FILE=build\src\version.h"
 if exist "%VERSION_FILE%" (
@@ -18,20 +28,34 @@ if exist "%VERSION_FILE%" (
     )
 )
 
-set "commit_message=%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_PATCH% build %BUILD_NUMBER% 배포"
+set "COMMIT_MESSAGE=%MODULE_NAME% %VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_PATCH% build %BUILD_NUMBER% 배포"
 
-if not exist lib\lbx\include\gl\       mkdir lib\lbx\include\gl\
-attrib -r /s /d lib\lbx\include\gl
-copy src\*.h lib\lbx\include\gl\
+if not exist "lib\lbx\include\%SUB_DIR%\"       mkdir "lib\lbx\include\%SUB_DIR%\"
+attrib -R "lib\lbx\include\%SUB_DIR%\*.h"
+copy src\*.h "lib\lbx\include\%SUB_DIR%\"
+attrib +R "lib\lbx\include\%SUB_DIR%\*.h"
 
 pushd lib\lbx
 git pull
-git add include/gl/*
+git add "include/%SUB_DIR%/*"
 git add lib/*
 
-if not commit_message == "" (
-git commit -m "%commit_message%"
-git push
+git status | findstr /C:"nothing to commit, working tree clean" > nul
+set "GIT_RESULT=%errorlevel%"
+if not %GIT_RESULT% equ 0 (
+    rem 변경 내용이 감지됨
+    if not COMMIT_MESSAGE == "" (
+        git commit -m "%COMMIT_MESSAGE%"
+        git push
+    ) 
 )
 popd
-lib\lbx\tools\inc VERSION_PATCH "%VERSION_FILE%"
+
+if %GIT_RESULT% equ 0 (
+    echo ### Nothing to deploy
+) else (
+    lib\lbx\tools\inc VERSION_PATCH "%VERSION_FILE%"
+    echo ### %COMMIT_MESSAGE%
+)
+
+@echo on
